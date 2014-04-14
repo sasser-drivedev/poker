57fd068ff99f5cb38d3fb3fd9810b724b160f4ee
John Sasser <sasser@drivedev.com>
2014-04-14 00:02:20 -0700

    update, some clean up

--- cards.py
+++ cards.py
@@ -3,46 +3,44 @@ from collections import Counter
 from operator import itemgetter
 
 #global variables
-x = 1
 deck = [] 
 hands=[]
 card=[]
 winners = []
+face_values = (2,3,4,5,6,7,8,9,10,11,12,13,14)
+players = 9
+suits = ('s','h','c','d')
+
+
 
 #construct and shuffle the deck
+print ''
 print "building 52-card deck..."
 print ''
-while  x < 14:
-    card = [x+1,'s']
-    deck.append(card)
-    card = []
-    card = [x+1,'h']
-    deck.append(card)
-    card = []
-    card = [x+1,'c']
-    deck.append(card)
-    card = []
-    card = [x+1,'d']
-    deck.append(card)
-    card = []
-    x += 1
+
+for i in face_values:
+    for j in suits:
+        card = [i,j]
+        deck.append(card)
+        card = []
+
 print deck
 print ''
 print "shuffling..."
 print ''
-
 random.shuffle(deck)
 print ''
 print deck
 print ''
 
 #deals player cards
-player = 0
-while player <9:
-    hand = ['seat' + str(player+1),deck[player],deck[player + 9]]
+i=0
+while i < players:
+    hand = ['seat' + str(i+1),deck[i],deck[i + 9]]
     hands.append(hand)
-    print hands[player]
-    player=player+1
+    print hands[i]
+    #player=player+1
+    i+=1
 
 #deals community cards
 community = [deck[21],deck[22],deck[23],deck[25],deck[27]]
@@ -51,9 +49,20 @@ print "community cards"
 print community 
 print ''
 
-s=1
+
+#check if a flush is possible
+community_flush =[]
+flush_possible = False
+for c in community:
+    community_flush.append(c[1])
+for c in suits:
+    if community_flush.count(c) >= 3:
+        flush_possible = True
+
+
 #constucts and evaluates hands
-while s < 10:
+s=1
+while s <= players:
     madehand = False
     winner = []
     #builds the 7-card hands
@@ -71,84 +80,82 @@ while s < 10:
     unique_straight_cards = set(straight_cards) 
     sorted_straight_cards = sorted(unique_straight_cards, reverse=True)
 
+    #insert straight flush evaluaor 
+
     #quads evaulator
     fourofakind=[]
     f=1
     while f <= len(sorted_straight_cards):
         if straight_cards.count(sorted_straight_cards[f-1]) == 4:
             fourofakind.append(sorted_straight_cards[f-1])
-        f = f+1
+        f +=1
     sorted(fourofakind, reverse=True)
     if len(fourofakind) >= 1:
         print "quad " + str(fourofakind[0]) + "'s"
         madehand = True
         winner = [s,2,fourofakind[0],0]
 
-        #else:
-        #print "no quads"
 
     #full house evaluator 
     boat=[]
     y=1
     while y <= len(sorted_straight_cards) and madehand == False:
         if straight_cards.count(sorted_straight_cards[y-1]) == 3:
-            #print "trip " + str(sorted_straight_cards[y-1])
             boat.append(sorted_straight_cards[y-1])
             j=1
             while j <= len(sorted_straight_cards):
                 if straight_cards.count(sorted_straight_cards[j-1]) >= 2 and sorted_straight_cards[j-1] != boat[0]:
                      boat.append(sorted_straight_cards[j-1])
-                j=j+1
-        y = y+1
-    #sorted(boat, reverse=True)
+                j+=1
+        y +=1
     if len(boat) >= 2:
         print  str(boat[0]) + " full of " + str(boat[1])
         madehand = True
         winner = [s,3,boat[0],boat[1]]   
-    #else:
-        #print "no boat"
+
+        
+    
 
     #flush evaluator 
-    i=1
-    spade_flush_cards = []
-    heart_flush_cards = []
-    diamond_flush_cards = []
-    club_flush_cards = []
-    while i <= 7 and madehand == False:
-        if seat1[i-1][1] == 's':
-            #spades = spades + 1
-            spade_flush_cards.append(seat1[i-1][0])
-        elif seat1[i-1][1] == 'd':
-            #diamonds = diamonds + 1
-            diamond_flush_cards.append(seat1[i-1][0])
-        elif seat1[i-1][1] == 'c':
-            #clubs = clubs + 1
-            club_flush_cards.append(seat1[i-1][0])
-        elif seat1[i-1][1] == 'h':
-            #hearts = hearts + 1
-            heart_flush_cards.append(seat1[i-1][0])
-        i += 1
-    if len(spade_flush_cards) >= 5:
-        print "spade flush (" + str(sorted(spade_flush_cards, reverse=True)[0]) + " high)"
-        winner = [s,4,(sorted(spade_flush_cards, reverse=True)[0]),0]
-        madehand = True
-    elif len(diamond_flush_cards) >= 5:
-        print "diamond flush (" + str(sorted(diamond_flush_cards, reverse=True)[0]) + " high)"
-        madehand = True
-        winner = [s,4,(sorted(diamond_flush_cards, reverse=True)[0]),0]
-    elif len(heart_flush_cards) >= 5:
-        print "heart flush (" + str(sorted(heart_flush_cards, reverse=True)[0]) + " high)"
-        madehand = True
-        winner = [s,4,(sorted(heart_flush_cards, reverse=True)[0]),0]
-    elif len(club_flush_cards) >= 5:
-        print "club flush (" + str(sorted(club_flush_cards, reverse=True)[0]) + " high)"
-        madehand = True
-        winner = [s,4,(sorted(club_flush_cards, reverse=True)[0]),0]
-    #else:
-        #print "no flush"
+    if flush_possible == True and madehand == False:
+        i=1
+        spade_flush_cards = []
+        heart_flush_cards = []
+        diamond_flush_cards = []
+        club_flush_cards = []
+    
+        while i <= 7 and madehand == False:
+            if seat1[i-1][1] == 's':
+                spade_flush_cards.append(seat1[i-1][0])
+            elif seat1[i-1][1] == 'd':
+                diamond_flush_cards.append(seat1[i-1][0])
+            elif seat1[i-1][1] == 'c':
+                club_flush_cards.append(seat1[i-1][0])
+            elif seat1[i-1][1] == 'h':
+                heart_flush_cards.append(seat1[i-1][0])
+            i += 1
+    
+            if len(spade_flush_cards) >= 5:
+                print "spade flush (" + str(sorted(spade_flush_cards, reverse=True)[0]) + " high)"
+                winner = [s,4,(sorted(spade_flush_cards, reverse=True)[0]),0]
+                madehand = True
+            elif len(diamond_flush_cards) >= 5:
+                print "diamond flush (" + str(sorted(diamond_flush_cards, reverse=True)[0]) + " high)"
+                madehand = True
+                winner = [s,4,(sorted(diamond_flush_cards, reverse=True)[0]),0]
+            elif len(heart_flush_cards) >= 5:
+                print "heart flush (" + str(sorted(heart_flush_cards, reverse=True)[0]) + " high)"
+                madehand = True
+                winner = [s,4,(sorted(heart_flush_cards, reverse=True)[0]),0]
+            elif len(club_flush_cards) >= 5:
+                print "club flush (" + str(sorted(club_flush_cards, reverse=True)[0]) + " high)"
+                madehand = True
+                winner = [s,4,(sorted(club_flush_cards, reverse=True)[0]),0]
 
     
     #straight evaluator 
+    
+
     if len(sorted_straight_cards) >= 5 and sorted_straight_cards[0] - sorted_straight_cards[1] == 1 and sorted_straight_cards[1] - sorted_straight_cards[2] == 1 and sorted_straight_cards[2] - sorted_straight_cards[3] == 1 and sorted_straight_cards[3] - sorted_straight_cards[4] == 1  and madehand == False:
         print str(sorted_straight_cards[0]) + " high straight"
         madehand = True
@@ -161,8 +168,7 @@ while s < 10:
         print str(sorted_straight_cards[2]) + " high straight"
         madehand = True
         winner = [s,5,sorted_straight_cards[2],0]
-   # else:
-        #print "no straight"
+
 
  
     #trips evaluator 
@@ -170,17 +176,16 @@ while s < 10:
     y=1
     while y <= len(sorted_straight_cards)  and madehand == False:
         if straight_cards.count(sorted_straight_cards[y-1]) == 3:
-            #print "trip " + str(sorted_straight_cards[y-1])
             trips.append(sorted_straight_cards[y-1])
-        y = y+1
+        y +=1
     sorted(trips, reverse=True)
     if len(trips) >= 1:
         print "trip " + str(trips[0])
         madehand = True
         winner = [s,6,trips[0],0]
-    #else:
-        #print "no trips"
 
+
+    #two pair evaluator
     twopair=[]
     z=1
     while z <= len(sorted_straight_cards) and madehand == False:
@@ -223,9 +228,6 @@ champian.sort(key=itemgetter(3), reverse=True)
 
 print "winner seat: " + str(champian[0][0])
 print ''
-
-
-
 
 
 
---
+++ test.py
@@ -0,0 +1,7 @@
+
+cards = (11,22,33,44)
+
+for i in cards:
+    print i
+
+
