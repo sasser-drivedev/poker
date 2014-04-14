e60682246bfedb7493510057d069f08d247ba628
John Sasser <sasser@drivedev.com>
2014-04-14 10:34:07 -0700

    more cleanup

--- cards.py
+++ cards.py
@@ -50,17 +50,23 @@ print community
 print ''
 
 
-#check if a flush is possible
+#check if a flush is possible and if the board is paired
 community_flush =[]
+community_pairs =[]
 flush_possible = False
+board_paired = False
 for c in community:
     community_flush.append(c[1])
+    community_pairs.append(c[0])
 for c in suits:
     if community_flush.count(c) >= 3:
         flush_possible = True
+for p in community_pairs:
+    if community_pairs.count(p) >= 2:
+        board_paired = True
 
 
-#constucts and evaluates hands
+#constucts the best possible 5-card poker hands for each player
 s=1
 while s <= players:
     madehand = False
@@ -80,7 +86,7 @@ while s <= players:
     unique_straight_cards = set(straight_cards) 
     sorted_straight_cards = sorted(unique_straight_cards, reverse=True)
 
-    #insert straight flush evaluaor 
+    #insert straight flush evaluator 
 
     #quads evaulator
     fourofakind=[]
@@ -97,24 +103,24 @@ while s <= players:
 
 
     #full house evaluator 
-    boat=[]
-    y=1
-    while y <= len(sorted_straight_cards) and madehand == False:
-        if straight_cards.count(sorted_straight_cards[y-1]) == 3:
-            boat.append(sorted_straight_cards[y-1])
-            j=1
-            while j <= len(sorted_straight_cards):
-                if straight_cards.count(sorted_straight_cards[j-1]) >= 2 and sorted_straight_cards[j-1] != boat[0]:
-                     boat.append(sorted_straight_cards[j-1])
-                j+=1
-        y +=1
-    if len(boat) >= 2:
-        print  str(boat[0]) + " full of " + str(boat[1])
-        madehand = True
-        winner = [s,3,boat[0],boat[1]]   
-
+    if  board_paired == True and madehand == False:
         
-    
+        boat=[]
+        y=1
+        while y <= len(sorted_straight_cards) and madehand == False:
+            if straight_cards.count(sorted_straight_cards[y-1]) == 3:
+                boat.append(sorted_straight_cards[y-1])
+                j=1
+                while j <= len(sorted_straight_cards):
+                    if straight_cards.count(sorted_straight_cards[j-1]) >= 2 and sorted_straight_cards[j-1] != boat[0]:
+                        boat.append(sorted_straight_cards[j-1])
+                    j+=1
+            y +=1
+            if len(boat) >= 2:
+                print  str(boat[0]) + " full of " + str(boat[1])
+                madehand = True
+                winner = [s,3,boat[0],boat[1]]   
+
 
     #flush evaluator 
     if flush_possible == True and madehand == False:
@@ -155,43 +161,39 @@ while s <= players:
     
     #straight evaluator 
     
-
-    if len(sorted_straight_cards) >= 5 and sorted_straight_cards[0] - sorted_straight_cards[1] == 1 and sorted_straight_cards[1] - sorted_straight_cards[2] == 1 and sorted_straight_cards[2] - sorted_straight_cards[3] == 1 and sorted_straight_cards[3] - sorted_straight_cards[4] == 1  and madehand == False:
-        print str(sorted_straight_cards[0]) + " high straight"
-        madehand = True
-        winner = [s,5,sorted_straight_cards[0],0]
-    elif len(sorted_straight_cards) >= 6 and sorted_straight_cards[1] - sorted_straight_cards[2] == 1 and sorted_straight_cards[2] - sorted_straight_cards[3] == 1 and sorted_straight_cards[3] - sorted_straight_cards[4] == 1 and sorted_straight_cards[4] - sorted_straight_cards[5] == 1  and madehand == False:
-        print str(sorted_straight_cards[1]) + " high straight"
-        madehand = True
-        winner = [s,5,sorted_straight_cards[1],0]
-    elif len(sorted_straight_cards) >= 7 and sorted_straight_cards[2] - sorted_straight_cards[3] == 1 and sorted_straight_cards[3] - sorted_straight_cards[4] == 1 and sorted_straight_cards[4] - sorted_straight_cards[5] == 1 and sorted_straight_cards[5] - sorted_straight_cards[6] == 1  and madehand == False:
-        print str(sorted_straight_cards[2]) + " high straight"
-        madehand = True
-        winner = [s,5,sorted_straight_cards[2],0]
-
+    z = 1
+    while z <= 3 and madehand == False:
+        
+        if len(sorted_straight_cards) >= z+4 and sorted_straight_cards[z-1] - sorted_straight_cards[z] == 1 and sorted_straight_cards[z] - sorted_straight_cards[z+1] == 1 and sorted_straight_cards[z+1] - sorted_straight_cards[z+2] == 1 and sorted_straight_cards[z+2] - sorted_straight_cards[z+3] == 1:
+            print str(sorted_straight_cards[z-1]) + " high straight"
+            madehand = True
+            winner = [s,5,sorted_straight_cards[z-1],0]
+        z+=1
 
  
     #trips evaluator 
-    trips=[]
-    y=1
-    while y <= len(sorted_straight_cards)  and madehand == False:
-        if straight_cards.count(sorted_straight_cards[y-1]) == 3:
-            trips.append(sorted_straight_cards[y-1])
-        y +=1
-    sorted(trips, reverse=True)
-    if len(trips) >= 1:
-        print "trip " + str(trips[0])
-        madehand = True
-        winner = [s,6,trips[0],0]
+    
+    if madehand == False:
+        trips=[]
+        y=1
+        while y <= len(sorted_straight_cards):
+            if straight_cards.count(sorted_straight_cards[y-1]) == 3:
+                trips.append(sorted_straight_cards[y-1])
+            y+=1
+            sorted(trips, reverse=True)
+            if len(trips) >= 1 and madehand == False:
+                print "trip " + str(trips[0])
+                madehand = True
+                winner = [s,6,trips[0],0]
 
 
-    #two pair evaluator
+    #two pair, pair, and high card evaluator
+    
     twopair=[]
     z=1
     while z <= len(sorted_straight_cards) and madehand == False:
         if straight_cards.count(sorted_straight_cards[z-1]) == 2:
             twopair.append(sorted_straight_cards[z-1])
-            #print "pair of " + str(sorted_straight_cards[z-1])
         z = z+1
 
     sorted(twopair, reverse=True)
@@ -208,13 +210,14 @@ while s <= players:
         madehand = True
         winner = [s,9,sorted_straight_cards[0],0]
 
-    s = s+ 1
+    s +=1 
     winners.append(winner)
     
 print ''
-#print winners
+
+#evaluates all hands and determines winner
+
 winners.sort(key=itemgetter(1))
-#print winners
 champian = [winners[0]]
 t=0
 while t < len(winners)-1:
@@ -223,9 +226,6 @@ while t < len(winners)-1:
     t+=1
 champian.sort(key=itemgetter(2), reverse=True)
 champian.sort(key=itemgetter(3), reverse=True)
-
-#print champian
-
 print "winner seat: " + str(champian[0][0])
 print ''
 
